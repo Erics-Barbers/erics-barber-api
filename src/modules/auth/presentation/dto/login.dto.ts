fd21f4dd75fc30c93c7f7955d3ea82f028090b44
Fahmidul Haque <fahmidulhaque97@pm.me>
2025-12-24 09:46:09 +0000

    add class-validators for passwords

--- src/modules/auth/presentation/dto/login.dto.ts
+++ src/modules/auth/presentation/dto/login.dto.ts
@@ -1,4 +1,9 @@
+import { IsEmail, Length } from 'class-validator';
+
 export class LoginDto {
+  @IsEmail()
   readonly email: string;
+
+  @Length(8, 20)
   readonly password: string;
 }
--- src/modules/auth/presentation/dto/register.dto.ts
+++ src/modules/auth/presentation/dto/register.dto.ts
@@ -1,4 +1,14 @@
+import { IsEmail, IsString, Length, Matches } from 'class-validator';
+
 export class RegisterDto {
+  @IsEmail()
   readonly email: string;
+
+  @IsString()
+  @Length(8, 20)
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{8,}$/, {
+    message:
+      'Password must be at least 8 characters long and contain at least one letter and one number',
+  })
   readonly passwordHash: string;
 }
