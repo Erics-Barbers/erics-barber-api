7fdba7ecfeea33088b1cc0a3addf1ded39f99a94
Fahmidul Haque <fahmidulhaque97@pm.me>
2026-02-13 16:36:47 +0000

    add config for clientUrl

--- src/modules/auth/presentation/dto/reset-password-email.dto.ts
+++ src/modules/auth/presentation/dto/reset-password-email.dto.ts
@@ -1,11 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsEmail } from 'class-validator';
 
 export class ResetPasswordEmailDto {
   @ApiProperty({
     description: 'Email address to send the reset password email to',
     example: 'user@example.com',
   })
-  @IsString()
+  @IsEmail()
   email: string;
 }
